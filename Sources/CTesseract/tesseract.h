b8778e93c0cae9eb220802d97eb11be52c938e4f
Alim Yuzbashev <45521753+alimyuz@users.noreply.github.com>
2025-01-06 12:20:46 +0100

    try removing headers

--- Sources/CTesseract/tesseract.h
+++ Sources/CTesseract/tesseract.h
@@ -6,14 +6,13 @@
 //
 
 #include <tesseract/capi.h>
-#include <tesseract/baseapi.h>
-#include <tesseract/export.h>
-#include <tesseract/ltrresultiterator.h>
-#include <tesseract/ocrclass.h>
-#include <tesseract/osdetect.h>
-#include <tesseract/pageiterator.h>
-#include <tesseract/publictypes.h>
-#include <tesseract/renderer.h>
-#include <tesseract/resultiterator.h>
-#include <tesseract/unichar.h>
-#include <tesseract/version.h>
+//#include <tesseract/export.h>
+//#include <tesseract/ltrresultiterator.h>
+//#include <tesseract/ocrclass.h>
+//#include <tesseract/osdetect.h>
+//#include <tesseract/pageiterator.h>
+//#include <tesseract/publictypes.h>
+//#include <tesseract/renderer.h>
+//#include <tesseract/resultiterator.h>
+//#include <tesseract/unichar.h>
+//#include <tesseract/version.h>
